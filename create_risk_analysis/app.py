360dff6d1c0b7730c455c6b14fd79a4518e23c07
Victor Bonilla <v-bonilla@users.noreply.github.com>
2020-05-19 16:13:36 +0200

    Integrate assess_risk func into create_risk_analysis

--- create_risk_analysis/app.py
+++ create_risk_analysis/app.py
@@ -6,15 +6,27 @@ assets_prefix = 'financial_data/'
 
 
 def lambda_handler(event, context):
-    body = json.loads(event.get('body'))
-    payload = {
-        'id': body.get('id'),
+    # body = json.loads(event.get('body'))
+    body = event.get('body')
+    lambda_client = boto3.client('lambda')
+    asset_id = body.get('id')
+    analyse_risk_payload = {
+        'id': asset_id,
         'ma_period': body.get('ma_period')
     }
-    lambda_client = boto3.client('lambda')
     new_analysis_uri_response = lambda_client.invoke(FunctionName='AnalyseRiskFunction',
-                                                     Payload=bytes(json.dumps(payload), encoding='utf8'))
+                                                     Payload=bytes(json.dumps(analyse_risk_payload), encoding='utf8'))
     new_analysis_id = json.loads(new_analysis_uri_response['Payload'].read().decode())
+    assess_risk_payload = {
+        "id": new_analysis_id,
+        "var_window": body.get('var_window'),
+        "mc_samples": body.get('mc_samples'),
+        "scalable_services": body.get('scalable_services'),
+        "parallel_resources": body.get('parallel_resources')
+    }
+    assess_risk_response = lambda_client.invoke(FunctionName='AssessRiskFunction',
+                                                     Payload=bytes(json.dumps(assess_risk_payload), encoding='utf8'))
+    new_analysis_id = json.loads(assess_risk_response['Payload'].read().decode())
     return {
         "statusCode": 201,
         "headers": {
