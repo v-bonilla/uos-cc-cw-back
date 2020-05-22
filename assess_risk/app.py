ebeed213cf4ec6365e474976da4aa13e052b042b
Victor Bonilla <v-bonilla@users.noreply.github.com>
2020-05-22 17:20:59 +0200

    Add scalable services functionality to calculate VAR in ec2 cluster

--- assess_risk/app.py
+++ assess_risk/app.py
@@ -6,11 +6,30 @@ import pandas as pd
 import s3fs
 import boto3
 
-s3_client = boto3.client('s3')
 bucket_name = os.environ.get('S3_BUCKET_NAME')
+credentials = '''[default]
+aws_access_key_id=ASIARADQARUSOC5YRZQH
+aws_secret_access_key=7k4lMAf32QfGqSM5eZzRK0PUn/3atBIne+AO/BRe
+aws_session_token=FwoGZXIvYXdzEAAaDK5yqCkQ3e/CRKZnqiLDAZCb7ekc4V+3gHJ8xe+GA4l6pAYEHwoSoJezhr+SpCNF2j2Ug4KD90cBTIHVVzzZqONS4qMfCaGAi571mxv/mOveful1qS8bKv5uHSrL1rwXUVrDbizrRtz2ZBMz2e1Lfoa1uiHrU+0qatG7lfoV+QrsPsLOkK7K57+jBXXQaADoaivwME8mQLbMIxaFpIfiEFV47zz0Y6rFZvstTsJc8RU2A2y+xfha/KX58tUpX9uOPttbKrMyClpR7C19BwgG9Fcs8yjRv5/2BTItuhax1hE88nkom2hOhi6ZluO0SmlKJKF5s2DZH0fos6FTVL60tir1n6wBa0xc'''
+user_data_ec2 = '''#!/bin/bash
+cd /root
+mkdir .aws
+cat > .aws/credentials << EOL
+{}
+EOL
+aws s3 cp s3://uos-cc-cw/ec2_resources ./ec2_resources --recursive
+yum install python3 -y
+python3 -m venv ./ec2_resources/
+cd ec2_resources
+source bin/activate
+pip3 install -r requirements.txt
+python3 assess_risk_ec2.py {} {} {} {}
+shutdown now'''
 
 
-def calculate_var(df, var_window, mc_samples):
+def calculate_var_window_stats(df, var_window):
+    df['vw_mean'] = np.nan
+    df['vw_std'] = np.nan
     var_win_scaled = var_window - 1
     for row in df.iterrows():
         index = row[0]
@@ -18,14 +37,40 @@ def calculate_var(df, var_window, mc_samples):
             series = row[1]
             if series['sig'] != 0:
                 price_window = df.loc[index - var_win_scaled:index, 'Adj Close']
-                change = price_window.rolling(window=2).apply(lambda x: (x.iloc[1] - x.iloc[0])/x.iloc[0]).dropna()
-                mc_series = np.random.normal(change.mean(), change.std(), mc_samples)
-                mc_series.sort()
-                if series['sig'] == 1:
-                    var_quantiles = np.quantile(mc_series, [0.95, 0.99])
-                else:
-                    var_quantiles = np.quantile(mc_series, [0.05, 0.01])
-                df.loc[index, ['var_95', 'var_99']] = [(1 + x) * series['Adj Close'] for x in var_quantiles]
+                change = price_window.rolling(window=2).apply(lambda x: (x.iloc[1] - x.iloc[0]) / x.iloc[0]).dropna()
+                df.loc[index, ['vw_mean', 'vw_std']] = [change.mean(), change.std()]
+
+
+def calculate_var(df, mc_samples):
+    df['var_95'] = np.nan
+    df['var_99'] = np.nan
+    for row in df.iterrows():
+        index = row[0]
+        series = row[1]
+        if series['vw_mean'] != np.nan and series['vw_std'] != np.nan:
+            mc_series = np.random.normal(series['vw_mean'], series['vw_std'], mc_samples)
+            mc_series.sort()
+            if series['sig'] == 1:
+                var_quantiles = np.quantile(mc_series, [0.95, 0.99])
+            else:
+                var_quantiles = np.quantile(mc_series, [0.05, 0.01])
+            df.loc[index, ['var_95', 'var_99']] = [(1 + x) * series['Adj Close'] for x in var_quantiles]
+
+
+def partitions_to_s3(partitions):
+    for id, partition in enumerate(partitions):
+        partition_s3_url = 's3://' + bucket_name + '/' + 'var_partitions/' + str(id) + '.csv'
+        partition.to_csv(partition_s3_url, index=False)
+
+
+def launch_cluster(parallel_resources, report_s3_url, mc_samples_ec2):
+    ec2_client = boto3.resource('ec2')
+    for id in range(parallel_resources):
+        ec2_client.create_instances(InstanceType='t2.micro', MinCount=1, MaxCount=1, ImageId='ami-0323c3dd2da7fb37d',
+                                    InstanceInitiatedShutdownBehavior='terminate', SecurityGroupIds=['sg-08f3f998506ef0548'],
+                                    KeyName='us-east-1-kp',
+                                    UserData=user_data_ec2.format(credentials, id, report_s3_url, parallel_resources,
+                                                                  mc_samples_ec2))
 
 
 def lambda_handler(event, context):
@@ -33,30 +78,28 @@ def lambda_handler(event, context):
     var_window = int(event.get('var_window'))
     mc_samples = int(event.get('mc_samples'))
     scalable_services = int(event.get('scalable_services'))
+    # Get risk analysis
+    payload = {
+        'pathParameters': {
+            'id': ra_id
+        }
+    }
+    lambda_client = boto3.client('lambda')
+    ra_response = lambda_client.invoke(FunctionName='BrowseRiskAnalysisFunction',
+                                       Payload=bytes(json.dumps(payload), encoding='utf8'))
+    ra = json.loads(json.loads(ra_response['Payload'].read().decode()).get('body'))
+    report_s3_url = 's3://' + bucket_name + '/' + 'risk_analyses/' + ra.get('id') + '_' + ra.get('report_name') + '.csv'
+    df = pd.read_json(json.dumps(ra.get('data')))
+    calculate_var_window_stats(df, var_window)
     if not scalable_services:
-        # Get risk analysis
-        payload = {
-            'pathParameters': {
-                'id': ra_id
-            }
-        }
-        lambda_client = boto3.client('lambda')
-        ra_response = lambda_client.invoke(FunctionName='BrowseRiskAnalysisFunction',
-                                           Payload=bytes(json.dumps(payload), encoding='utf8'))
-        ra = json.loads(json.loads(ra_response['Payload'].read().decode()).get('body'))
-        df = pd.read_json(json.dumps(ra.get('data')))
-        # Calculate VAR
-        df['var_95'] = np.nan
-        df['var_99'] = np.nan
-        calculate_var(df, var_window, mc_samples)
+        calculate_var(df, mc_samples)
         # Store results
-        report_s3_url = 's3://' + bucket_name + '/' + 'risk_analyses/' + ra.get('id') + '_' + ra.get('report_name') + '.csv'
         df.to_csv(report_s3_url, index=False)
-        return {
-            "status": 200
-        }
     else:
         parallel_resources = int(event.get('parallel_resources'))
-        # launch EMR. code file stored in s3?
-        pass
-
+        # Store partial results for ec2 instances
+        df.to_csv(report_s3_url, index=False)
+        launch_cluster(parallel_resources, report_s3_url, int(mc_samples / parallel_resources))
+    return {
+        "status": 200
+    }
---
+++ assess_risk_ec2.py
@@ -0,0 +1,100 @@
+import sys
+
+import boto3
+import numpy as np
+import pandas as pd
+import s3fs
+
+bucket_name = 'uos-cc-cw'
+var_results_url = 's3://' + bucket_name + '/var_results/'
+
+
+def calculate_var(df, mc_samples_ec2):
+    print('Calculating VAR...')
+    v95 = 'var_95_' + str(id)
+    v99 = 'var_99_' + str(id)
+    df[v95] = np.nan
+    df[v99] = np.nan
+    for row in df.iterrows():
+        index = row[0]
+        series = row[1]
+        if series['vw_mean'] != np.nan and series['vw_std'] != np.nan:
+            mc_series = np.random.normal(series['vw_mean'], series['vw_std'], mc_samples_ec2)
+            mc_series.sort()
+            if series['sig'] == 1:
+                var_quantiles = np.quantile(mc_series, [0.95, 0.99])
+            else:
+                var_quantiles = np.quantile(mc_series, [0.05, 0.01])
+            df.loc[index, [v95, v99]] = [(1 + x) * series['Adj Close'] for x in var_quantiles]
+
+
+def get_s3_objects(s3_client, bucket_name, **base_kwargs):
+    continuation_token = None
+    while True:
+        list_kwargs = dict(Bucket=bucket_name, MaxKeys=10000, **base_kwargs)
+        if continuation_token:
+            list_kwargs['ContinuationToken'] = continuation_token
+        response = s3_client.list_objects_v2(**list_kwargs)
+        yield from response.get('Contents', [])
+        if not response.get('IsTruncated'):
+            break
+        continuation_token = response.get('NextContinuationToken')
+
+
+def list_s3_objects(s3_client, prefix):
+    return [s3_object.get('Key') for s3_object in get_s3_objects(s3_client, bucket_name, Prefix=prefix) if
+            not s3_object['Key'] == prefix]
+
+
+def delete_s3_objects(s3_client, key_list):
+    objects = [{'Key': k} for k in key_list]
+    delete_params = {'Objects': objects}
+    s3_client.delete_objects(Bucket=bucket_name, Delete=delete_params)
+
+
+def merge_var_results(df, parallel_resources):
+    print('Merging results from other ec2 instances...')
+    df_res = df.copy()
+    df_res.rename(columns={'var_95_0': 'var_95', 'var_99_0': 'var_99'}, inplace=True)
+    s3_client = boto3.client('s3')
+    # Wait for results in S3
+    var_results_ready = False
+    while not var_results_ready:
+        var_results = list_s3_objects(s3_client, 'var_results/')
+        if len(var_results) == parallel_resources - 1:
+            var_results_ready = True
+    # Merge results
+    for id in range(1, parallel_resources):
+        var95_col = 'var_95_' + str(id)
+        var99_col = 'var_99_' + str(id)
+        cols_to_merge = ['Date', var95_col, var99_col]
+        df = pd.read_csv(var_results_url + str(id) + '.csv')
+        df_res = df_res.merge(df[cols_to_merge], on='Date')  # To ensure integrity with Date
+        df_res['var_95'] = df_res['var_95'] + df_res[var95_col]
+        df_res['var_99'] = df_res['var_99'] + df_res[var99_col]
+        df_res.drop(columns=[var95_col, var99_col], inplace=True)
+    df_res['var_95'] = df_res['var_95'] / parallel_resources
+    df_res['var_99'] = df_res['var_99'] / parallel_resources
+    # Delete results from S3
+    delete_s3_objects(s3_client, var_results)
+    return df_res
+
+
+def run(id, report_s3_url, parallel_resources, mc_samples_ec2):
+    print('Assess risk started...')
+    df = pd.read_csv(report_s3_url)
+    calculate_var(df, mc_samples_ec2)
+    if id == 0:
+        df_res = merge_var_results(df, parallel_resources)
+        df_res.to_csv(report_s3_url, index=False)
+    else:
+        df.to_csv(var_results_url + str(id) + '.csv', index=False)
+    print('Done.')
+
+
+if __name__ == "__main__":
+    id = int(sys.argv[1])
+    report_s3_url = str(sys.argv[2])
+    parallel_resources = int(sys.argv[3])
+    mc_samples_ec2 = int(sys.argv[4])
+    run(id, report_s3_url, parallel_resources, mc_samples_ec2)
